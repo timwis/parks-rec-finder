7f83cc74dacf3005274a519fe20b8e7b89c26b58
Ben Dolly <bromeodolly@gmail.com>
2017-11-12 19:50:53 -0500

    adds browser plugins to karma config

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['PhantomJS', 'Chrome', 'Firefox', 'Safari'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['super-dots', 'live-html'],
     files: ['./index.js'],
@@ -32,12 +32,42 @@ module.exports = function (config) {
     webpackMiddleware: {
       noInfo: true
     },
+
     coverageReporter: {
       dir: './coverage',
       reporters: [
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' }
       ]
+    },
+
+    browserStack: {
+      username: 'dataatphilagov1',
+      accessKey: 'RqcCFrLbmrV5mZzQCPfq'
+    },
+
+    customLaunchers: {
+      IE9: {
+        base: 'IE',
+        'x-ua-compatible': 'IE=EmulateIE9'
+      },
+      IE8: {
+        base: 'IE',
+        'x-ua-compatible': 'IE=EmulateIE8'
+      },
+      bs_firefox_mac: {
+        base: 'BrowserStack',
+        browser: 'firefox',
+        browser_version: '21.0',
+        os: 'OS X',
+        os_version: 'Mountain Lion'
+      },
+      bs_iphone5: {
+        base: 'BrowserStack',
+        device: 'iPhone 5',
+        os: 'ios',
+        os_version: '6.0'
+      }
     }
   })
 }
