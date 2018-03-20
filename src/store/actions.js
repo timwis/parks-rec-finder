1b914dd3cc16d63ba40b6ad31eab0de76482d3ce
timwis <tim@timwis.com>
2018-03-20 06:23:03 -0400

    Rename "api" to "carto"

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,9 @@
-import Api from '~/api'
+import Carto from '~/api/carto'
 
-const ENDPOINT = `https://phl.carto.com/api/v2/sql`
-const api = new Api(ENDPOINT)
+const CARTO_ENDPOINT = `https://phl.carto.com/api/v2/sql`
+const carto = new Carto(CARTO_ENDPOINT)
 
 export async function getActivityCategories ({ commit }) {
-  const activityCategories = await api.getActivityCategories()
+  const activityCategories = await carto.getActivityCategories()
   commit('SET_ACTIVITY_CATEGORIES', activityCategories)
 }
