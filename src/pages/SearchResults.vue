eca5321a45059368aae713081def371c47414004
timwis <tim@timwis.com>
2018-03-21 14:22:09 -0400

    Show search results in list on search page

--- src/pages/SearchResults.vue
+++ src/pages/SearchResults.vue
@@ -5,15 +5,64 @@
       Showing {{ count }} results
       for {{ term }}
     </p>
+
+    <ul>
+      <li>
+        <router-link :to="{ path: '/search/activities', query: { term } }">
+          Activities ({{ activities.length }})
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ path: '/search/locations', query: { term } }">
+          Locations ({{ locations.length }})
+        </router-link>
+      </li>
+    </ul>
+
+    <ul v-if="activeTab === 'activities'">
+      <ActivityListItem
+        v-for="activity in activities"
+        :key="activity.id"
+        :id="activity.id"
+        :name="activity.name"
+        :fee="activity.fee"
+        :fee-frequency="activity.feeFrequency"
+        :gender="activity.gender"
+        :age-low="activity.ageLow"
+        :age-high="activity.ageHigh"
+        :facility-name="activity.facilityName"
+        :facility-address="activity.facilityAddress"
+      />
+    </ul>
+
+    <ul v-else-if="activeTab === 'locations'">
+      <LocationListItem
+        v-for="location in locations"
+        :key="location.id"
+        :id="location.id"
+        :name="location.name"
+        :address="location.address"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions, mapMutations } from 'vuex'
+import ActivityListItem from '~/components/ActivityListItem'
+import LocationListItem from '~/components/LocationListItem'
 
 export default {
   props: {
+    activeTab: {
+      type: String, // activities or programs
+      default: 'activities'
+    },
     term: String
+  },
+  components: {
+    ActivityListItem,
+    LocationListItem
   },
   computed: {
     ...mapState([
@@ -33,10 +82,27 @@ export default {
     this.getActivities({ term })
     this.getLocations({ term })
   },
-  methods: mapActions([
-    'getActivities',
-    'getLocations'
-  ])
+  destroyed () {
+    this.resetActivities()
+    this.resetLocations()
+  },
+  watch: {
+    term () {
+      const term = this.term
+      this.getActivities({ term })
+      this.getLocations({ term })
+    }
+  },
+  methods: {
+    ...mapActions([
+      'getActivities',
+      'getLocations'
+    ]),
+    ...mapMutations({
+      'resetActivities': 'RESET_ACTIVITIES',
+      'resetLocations': 'RESET_LOCATIONS'
+    })
+  }
 }
 </script>
 
