2ee589077b8a5debd54f5a15df1f911f440f9536
timwis <tim@timwis.com>
2018-03-28 06:33:00 -0400

    Reorder store for more consistency

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -16,44 +16,36 @@ export function SET_LOCATION_CATEGORIES (state, locationCategories) {
   state.locationCategories = locationCategories
 }
 
-export function SET_LOCATION_CATEGORY_DETAILS (state, locationCategoryDetails) {
-  state.locationCategoryDetails = locationCategoryDetails
-}
-
-export function SET_LOCATIONS (state, locations) {
-  state.locations = locations
-}
-
-export function RESET_LOCATION_CATEGORY_DETAILS (state) {
-  state.locationCategoryDetails = {}
-}
-
-export function RESET_LOCATIONS (state) {
-  state.locations = []
-}
-
 export function SET_ACTIVITY_CATEGORY_DETAILS (state, activityCategoryDetails) {
   state.activityCategoryDetails = activityCategoryDetails
-}
-
-export function SET_ACTIVITIES (state, activities) {
-  state.activities = activities
 }
 
 export function RESET_ACTIVITY_CATEGORY_DETAILS (state) {
   state.activityCategoryDetails = {}
 }
 
+export function SET_LOCATION_CATEGORY_DETAILS (state, locationCategoryDetails) {
+  state.locationCategoryDetails = locationCategoryDetails
+}
+
+export function RESET_LOCATION_CATEGORY_DETAILS (state) {
+  state.locationCategoryDetails = {}
+}
+
+export function SET_ACTIVITIES (state, activities) {
+  state.activities = activities
+}
+
 export function RESET_ACTIVITIES (state) {
   state.activities = []
 }
 
-export function SET_LOCATION_DETAILS (state, locationDetails) {
-  state.locationDetails = locationDetails
+export function SET_LOCATIONS (state, locations) {
+  state.locations = locations
 }
 
-export function RESET_LOCATION_DETAILS (state) {
-  state.locationDetails = {}
+export function RESET_LOCATIONS (state) {
+  state.locations = []
 }
 
 export function SET_ACTIVITY_DETAILS (state, activityDetails) {
@@ -62,6 +54,14 @@ export function SET_ACTIVITY_DETAILS (state, activityDetails) {
 
 export function RESET_ACTIVITY_DETAILS (state) {
   state.activityDetails = {}
+}
+
+export function SET_LOCATION_DETAILS (state, locationDetails) {
+  state.locationDetails = locationDetails
+}
+
+export function RESET_LOCATION_DETAILS (state) {
+  state.locationDetails = {}
 }
 
 export function SET_SEARCH_LOCATION_GEOMETRY (state, geometry) {
