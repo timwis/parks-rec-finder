808e5add0d5936b594b14fdcd6998d34896d2a4d
timwis <tim@timwis.com>
2018-03-28 06:38:50 -0400

    Use object syntax in actions and mutations This is less verbose than a bunch of exports, and separate exports don't really add value.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,73 +1,75 @@
 import Vue from 'vue'
 
-export function BEGIN_REQUEST (state, requestName) {
-  Vue.set(state.pendingRequests, requestName, true)
-}
+export default {
+  BEGIN_REQUEST (state, requestName) {
+    Vue.set(state.pendingRequests, requestName, true)
+  },
 
-export function END_REQUEST (state, requestName) {
-  Vue.delete(state.pendingRequests, requestName)
-}
+  END_REQUEST (state, requestName) {
+    Vue.delete(state.pendingRequests, requestName)
+  },
 
-export function SET_ACTIVITY_CATEGORIES (state, activityCategories) {
-  state.activityCategories = activityCategories
-}
+  SET_ACTIVITY_CATEGORIES (state, activityCategories) {
+    state.activityCategories = activityCategories
+  },
 
-export function SET_LOCATION_CATEGORIES (state, locationCategories) {
-  state.locationCategories = locationCategories
-}
+  SET_LOCATION_CATEGORIES (state, locationCategories) {
+    state.locationCategories = locationCategories
+  },
 
-export function SET_ACTIVITY_CATEGORY_DETAILS (state, activityCategoryDetails) {
-  state.activityCategoryDetails = activityCategoryDetails
-}
+  SET_ACTIVITY_CATEGORY_DETAILS (state, activityCategoryDetails) {
+    state.activityCategoryDetails = activityCategoryDetails
+  },
 
-export function RESET_ACTIVITY_CATEGORY_DETAILS (state) {
-  state.activityCategoryDetails = {}
-}
+  RESET_ACTIVITY_CATEGORY_DETAILS (state) {
+    state.activityCategoryDetails = {}
+  },
 
-export function SET_LOCATION_CATEGORY_DETAILS (state, locationCategoryDetails) {
-  state.locationCategoryDetails = locationCategoryDetails
-}
+  SET_LOCATION_CATEGORY_DETAILS (state, locationCategoryDetails) {
+    state.locationCategoryDetails = locationCategoryDetails
+  },
 
-export function RESET_LOCATION_CATEGORY_DETAILS (state) {
-  state.locationCategoryDetails = {}
-}
+  RESET_LOCATION_CATEGORY_DETAILS (state) {
+    state.locationCategoryDetails = {}
+  },
 
-export function SET_ACTIVITIES (state, activities) {
-  state.activities = activities
-}
+  SET_ACTIVITIES (state, activities) {
+    state.activities = activities
+  },
 
-export function RESET_ACTIVITIES (state) {
-  state.activities = []
-}
+  RESET_ACTIVITIES (state) {
+    state.activities = []
+  },
 
-export function SET_LOCATIONS (state, locations) {
-  state.locations = locations
-}
+  SET_LOCATIONS (state, locations) {
+    state.locations = locations
+  },
 
-export function RESET_LOCATIONS (state) {
-  state.locations = []
-}
+  RESET_LOCATIONS (state) {
+    state.locations = []
+  },
 
-export function SET_ACTIVITY_DETAILS (state, activityDetails) {
-  state.activityDetails = activityDetails
-}
+  SET_ACTIVITY_DETAILS (state, activityDetails) {
+    state.activityDetails = activityDetails
+  },
 
-export function RESET_ACTIVITY_DETAILS (state) {
-  state.activityDetails = {}
-}
+  RESET_ACTIVITY_DETAILS (state) {
+    state.activityDetails = {}
+  },
 
-export function SET_LOCATION_DETAILS (state, locationDetails) {
-  state.locationDetails = locationDetails
-}
+  SET_LOCATION_DETAILS (state, locationDetails) {
+    state.locationDetails = locationDetails
+  },
 
-export function RESET_LOCATION_DETAILS (state) {
-  state.locationDetails = {}
-}
+  RESET_LOCATION_DETAILS (state) {
+    state.locationDetails = {}
+  },
 
-export function SET_SEARCH_LOCATION_GEOMETRY (state, geometry) {
-  state.searchLocationGeometry = geometry
-}
+  SET_SEARCH_LOCATION_GEOMETRY (state, geometry) {
+    state.searchLocationGeometry = geometry
+  },
 
-export function RESET_SEARCH_LOCATION_GEOMETRY (state) {
-  state.searchLocationGeometry = null
+  RESET_SEARCH_LOCATION_GEOMETRY (state) {
+    state.searchLocationGeometry = null
+  }
 }
