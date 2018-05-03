ec4d497c826051c813c3e103b198dabe1afe2ad3
Karissa <k@karissademi.com>
2018-05-03 10:51:30 -0400

    Correct mobile views, esp. for very small screens

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -144,8 +144,17 @@ export default {
 input[type="search"]
   margin-bottom: 0
   font-size: 1rem
+  @media screen and (max-width: 39.9375em)
+    font-size: .85rem
+
 .bdr-right
   border-right: 1px solid #444
+
+.page-title
+  font-weight: bold
+.page-subtitle
+  font-weight: normal
+
 #application
   .site-header
     .logo
@@ -156,23 +165,29 @@ input[type="search"]
       margin: 0
     .page-title-container h1
       font-size: 2rem
+
+//clean up display on very small screens
+@media screen and (max-width: 39.9375em)
+  #application
+    .site-header
+      padding: .5rem 0
+      .page-title-container
+        margin-bottom: .5rem
+        margin-left: 0
+        h2
+          font-size: .85rem
+//account for extremely small screens > 360px wide
+@media screen and (max-width: 25.71rem)
+  .page-title-container a
+    max-width: 17.86rem
+
+
 .mobile-menu-toggle
   color: white
 
 .mobile-menu
   width: 100%
   height: 100vh
-
-.page-title
-  font-weight: bold
-.page-subtitle
-  font-weight: normal
-
-@media screen and (max-width: 39.9375em)
-  .page-title-container
-    margin-bottom: 1rem
-
-.mobile-menu
   a:link, a:visited
     color: white
     text-transform: uppercase
